4022e5ff741ddbfde5aaefd88fd112b26bf22813
phihd <phictin@gmail.com>
2024-04-17 12:43:00 +0700

    check specific issueId is allowed

--- backend/schedules/jobs/dueIssueCheck.js
+++ backend/schedules/jobs/dueIssueCheck.js
@@ -2,30 +2,37 @@ const Issue = require('../../models/issue')
 const sendEmailNotification = require('../../notification/email')
 const createAppNotification = require('../../notification/app')
 
-const performDueIssueCheck = async () => {
+const performDueIssueCheck = async (issueId = null) => {
   console.log('Performing the due issue check')
   try {
     const now = new Date()
     const twentyFourHoursLater = new Date(now.getTime() + 24 * 60 * 60 * 1000)
 
-    const issuesDueSoon = await Issue.find({
+    const query = {
       dueDate: { $gte: now, $lte: twentyFourHoursLater },
-      status: 'Open',  
-      isNotified: { $ne: true }
-    }).populate('assignees', 'email name')
+      status: 'Open',
+    }
+    if (issueId) {
+      // Specific issue query without considering 'isNotified' status
+      query = { _id: issueId }
+    } else {
+      // General query includes only issues not yet notified
+      query.isNotified = { $ne: true }
+    }
 
-    for (const issue of issuesDueSoon) {
-      const notificationPromises = issue.assignees.map(async (assignee) => {
+    const issuesDueSoon = await Issue.find(query).populate('assignees', 'email name')
+
+    await Promise.all(issuesDueSoon.map(async (issue) => {
+      await Promise.all(issue.assignees.map(async (assignee) => {
         if (assignee.email) {
           await sendEmailNotification(assignee.email, `Issue "${issue.title}" is due soon.`)
           await createAppNotification(assignee.id, `Issue "${issue.title}" is due within 24 hours.`)
         }
-      })
-
-      await Promise.all(notificationPromises)
+      }))
 
       await Issue.findByIdAndUpdate(issue._id, { isNotified: true })
-    }
+    }))
+
   } catch (error) {
     console.error('Error during due issue check:', error)
   }
