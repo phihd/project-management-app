14c65db72ea17d726a6050810431497bc3b83212
X1 <vo.nguyen@aiesec.net>
2024-04-26 09:57:06 +0700

    ProjectDetail page interface update

--- frontend/src/components/ProjectDetail.js
+++ frontend/src/components/ProjectDetail.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import React, { useState, useEffect, useContext } from 'react'
 import './ProjectDetail.css'
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 import { useQuery } from 'react-query'
 import projectService from '../services/projects'
 import issueService from '../services/issues'
@@ -9,11 +9,20 @@ import UserContext from './UserContext'
 import NewIssueForm from './NewIssueForm'
 
 import delete_button from '../img/delete.png'
+import dept_button from '../img/department_button.png'
+import status_button from '../img/status_button.png'
+import new_issue from '../img/new_issue_btn.png'
+import open_status from '../img/open_issue.png'
+import close_status from '../img/close_issue.png'
+import comment_count from '../img/comment_count.png'
 
 function ProjectDetail({ projects }) {
   const { projectId } = useParams()
   const { user } = useContext(UserContext)
   const [showIssueForm, setShowIssueForm] = React.useState(false)
+  const [openIssues, setOpenIssues] = useState()
+  const [sortedIssues, setSortedIssues] = useState()
+  const navigate = useNavigate()
 
   // Fetch project details
   const {
@@ -32,7 +41,19 @@ function ProjectDetail({ projects }) {
     isError: isIssuesError,
     error: issuesError
   } = useQuery(['issues', projectId], () => issueService.getAll(projectId), {
-    enabled: !!project
+    enabled: !!project,
+    onSuccess: (data) => {
+      setOpenIssues(data.filter(issue => issue.status === "Open"))
+      setSortedIssues(data.sort((a, b) => {
+        if (a.status === 'Open' && b.status !== 'Open') {
+          return -1
+        }
+        if (a.status !== 'Open' && b.status === 'Open') {
+          return 1 
+        }
+        return 0
+      }))
+    }
   })
 
 
@@ -42,6 +63,10 @@ function ProjectDetail({ projects }) {
 
   const handleCloseForm = () => {
     setShowIssueForm(false)
+  }
+
+  const handleNavigateToIssue = (issueId) => {
+    navigate(`/project/${projectId}/${issueId}`)
   }
 
   const handleDeleteIssue = async (issueId, event) => {
@@ -73,8 +98,14 @@ function ProjectDetail({ projects }) {
         <h1 className="project-title">{project.name}</h1>
         <div className="header-buttons">
           {/* <button>{project.status.activityStatus}</button> */}
-          <button> Tổ Chức Hành Chính </button>
-          <button> Status </button>
+          <button> 
+            <img src={dept_button} alt="Department" className="button-img" />
+            Phòng Tổ Chức Hành Chính 
+          </button>
+          <button>
+            <img src={status_button} alt="Status" className="button-img" />
+            Status 
+          </button>
         </div>
       </div>
       <div className="project-info">
@@ -98,26 +129,44 @@ function ProjectDetail({ projects }) {
         </div>
       </div>
       <div className="project-issues">
+      <div className="issues-header">
         <h2>Project Issues</h2>
-        <div className="issue-controls">
-          <span>{issues.length} Open Issues</span>
-          <button onClick={handleNewIssueClick}>+ New Issue</button>
-        </div>
-        <ul className="issue-list">
-          {issues.map((issue) => (
-            <li key={issue.id} className="issue-item">
-              <div>
-                <h4>{issue.title}</h4>
-                <p>Status: {issue.status}</p>
-                <p>{issue.description}</p>
-                <p>Comments: {issue.comments.length}</p>
+        <span className="issue-count">{openIssues?.length} open issues</span>
+        <button className='new-issue-btn' onClick={handleNewIssueClick}>
+          <img src={new_issue} alt="Add New Issue" className="issue-button-img" />
+          New Issue
+        </button>
+      </div>
+      <ul className="issue-list">
+        {sortedIssues?.map(issue => (
+          <li key={issue.id} className={`issue-item ${issue.status.toLowerCase()}`}>
+            <div className="issue-content" onClick={() => handleNavigateToIssue(issue.id)}>
+              <h4>{issue.title}</h4>
+              <button className={`status-btn ${issue.status.toLowerCase()}`}>
+                <img src={issue.status === 'Open' ? open_status : close_status} alt={issue.status} />
+                {issue.status}
+              </button>
+            </div>
+            {issue.description ? (
+              issue.description.split('\n').map((line, index) => (
+                <p key={index}>{line}</p>
+              ))
+            ) : (
+              <p>No description available</p>
+            )}
+            <hr />
+            <div className="comment-delete-container">
+              <div className="comment-count">
+                <img src={comment_count} alt="Comments" className="comment-img" />
+                {issue.comments.length}
               </div>
               <button className='delete-button' onClick={(e) => handleDeleteIssue(issue.id, e)}>
                 <img className='delete-button-img' src={delete_button} alt="Delete" />
               </button>
-            </li>
-          ))}
-        </ul>
+            </div>
+          </li>
+        ))}
+      </ul>
       </div>
       {showIssueForm && (
         <div className="overlay">
